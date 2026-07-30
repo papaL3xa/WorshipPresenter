2bbe6a237d3723491d80e476ceeb88e2db899a81
Worship Presenter Admin <admin@worshippresenter.local>
2026-07-30 17:53:21 +0700

    Auto deploy update: 2026-07-30 17:53:21

--- test_api.js
+++
@@ -1,1 +0,0 @@
-fetch('https://script.google.com/macros/s/AKfycbxtiocm9f0K_39AP1dfTadWuHoVLgCMsyTUodm0d9jeO1TRcchbnvm5cpiQd2v8k-dJ/exec?action=getPlaylists').then(r=>r.text()).then(console.log).catch(console.error);
