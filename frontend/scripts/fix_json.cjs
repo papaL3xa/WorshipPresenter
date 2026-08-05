1b4e1d54b09b0d7b1a740535449466c5e23b289d
Worship Presenter Admin <admin@worshippresenter.local>
2026-08-05 17:15:39 +0700

    Update Desktop App: 2026-08-05 17:15:39

--- frontend/scripts/fix_json.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('frontend/scripts/update_songs.json', 'utf8');
-data = data.replace('\\ Akan kering lautan.', '\\nAkan kering lautan.');
-fs.writeFileSync('frontend/scripts/update_songs.json', data);
-console.log('Fixed JSON');
